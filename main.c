05d8bafef7fa7b4f74461df040d5bcd53d7f0814
Brian Wong <brwwong@cs.stonybrook.edu>
2018-02-24 16:13:38 -0800

    can traverse through lists now i think

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "helper.h"
 
+typedef struct {
+	uint64_t allocated : 1;
+	uint64_t app_id : 2;
+	uint64_t size : 61 ;
+
+} header;
+
 int main(int argc, char** argv) {
     if (*(argv + 1) == NULL) {
         printf("You should provide name of the test file.\n");
@@ -12,8 +19,17 @@ int main(int argc, char** argv) {
      * You code goes below. Do not modify provided parts
      * of the code.
      */
-
-
+	int size;
+	int id;
+	do{
+	header *head= (header*)ram;
+	size= (head->size)<<3;
+	id= (head->app_id);
+	printf("size of block is %d\n", size);
+	printf("block is allocated is %d \n", head->allocated);
+	printf("block id is %d \n\n", head->app_id);
+	ram+=size;
+	}while(id!=0);
     /*
      * Do not modify code below.
      */
