79baef0978363275aa08b70d39d188a13128c50a
Brian Wong <brwwong@cs.stonybrook.edu>
2018-02-26 21:36:56 -0800

    sort by id should work now?

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "helper.h"
-
+#include "errno.h"
+#include "string.h"
 typedef struct {
 	uint64_t allocated : 1;
 	uint64_t app_id : 2;
@@ -19,17 +20,73 @@ int main(int argc, char** argv) {
      * You code goes below. Do not modify provided parts
      * of the code.
      */
+	if(ram==NULL){
+		printf("INIT_ERROR");
+		return errno;
+	}
+	if(tmp_buf==NULL){
+		printf("INIT_ERROR");
+		return errno;
+	}
 	int size;
 	int id;
+	int totalSize=0;
+	void *ram_pntr=ram;
+	header *head;
 	do{
-	header *head= (header*)ram;
-	size= (head->size)<<3;
-	id= (head->app_id);
-	printf("size of block is %d\n", size);
-	printf("block is allocated is %d \n", head->allocated);
-	printf("block id is %d \n\n", head->app_id);
-	ram+=size;
+		head= (header*)ram_pntr;
+		size= (head->size)<<3;
+		id= (head->app_id);
+		printf("size of block is %d\n", size);
+		printf("block is allocated is %d \n", head->allocated);
+		printf("block id is %d \n\n", head->app_id);
+		ram_pntr+=size;
+		totalSize+=size;
 	}while(id!=0);
+	if(totalSize>128){
+		if(cse320_sbrk(totalSize-128)==NULL){
+			printf("SBRK_ERROR\n");
+			return errno;
+			
+		}
+	}
+	//FIRST SORY BY ID, IDs can only go up to 1 anyway
+	void* tmp_pntr=tmp_buf;
+	int currID=1;
+	while(currID<4){
+	ram_pntr=ram;
+		do{
+			head= (header*)ram_pntr;
+			size= (head->size)<<3;
+			id= (head->app_id);
+			if(id==currID){
+			
+								
+				//put into tempbuf
+				memcpy(tmp_pntr,ram_pntr,head->size);
+				tmp_pntr+=(head->size);
+				printf("size of block is %d\n", size);
+				printf("block is allocated is %d \n", head->allocated);
+				printf("block id is %d \n\n", head->app_id);
+
+			}
+			ram_pntr+=size;
+			totalSize+=size;
+		}while(id!=0);
+	/*	while(head->app_id!=0){
+			head=(header*)ram_pntr;
+			if((head->app_id)==currID){
+				//put into tempbuf
+				memcpy(tmp_pntr,ram_pntr,head->size);
+				tmp_pntr+=(head->size);
+				printf("temp block id= %d \n", head->app_id);
+			}
+			ram_pntr+=(head->size);			
+		}
+	*/
+	currID++;
+	}
+
     /*
      * Do not modify code below.
      */
